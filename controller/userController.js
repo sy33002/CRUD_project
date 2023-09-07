4e31b841b94f80d53aa94306dabd53aafda5ff1a
sy33002 <sy33002@naver.com>
2023-09-07 15:32:41 +0900

    feat: user controller 수정

--- controller/userController.js
+++ controller/userController.js
@@ -13,62 +13,75 @@ exports.getSignup = (req, res) => {
   res.render('signup');
 };
 
-exports.getSignin = async (req, res) => {
-  const {name, id} = req.session.userInfo;
-  const result = await User.findOne({
-    where: { userid: id }
-  });
-  res.render('profile', {data: result.dataValues});
-};
-
-//회원가입
-exports.postSignup = async (req, res) => {
-  let { userid, pw, name } = req.body;
-  pw = bcryptPassword(pw);
-  const result = await User.create({
-    userid,
-    name,
-    pw,
-  });
-  res.send({result: true});
-};
-  
+// 로그인
 exports.postLogin = async (req, res) => {
-  const { userid, pw } = req.body;
+  const { user_id, user_pw } = req.body;
   const result = await User.findOne({
-    where: { userid: userid }
+    where: { user_id: user_id }
   });
   if (result) {
-    if (compareFunc(pw, result.pw) === true) {
-        req.session.userInfo = {
-        name: result.dataValues.name,
-        id: result.dataValues.userid,
-      }
-      const data = req.session.userInfo;
+    if (compareFunc(user_pw, result.user_pw) === true) {
+      req.session.userInfo = {
+      id: result.dataValues.id,
+      user_name: result.dataValues.user_name,
+      user_id: result.dataValues.user_id,
+      user_pw: result.dataValues.user_pw,
+      user_addr: result.dataValues.user_addr,
+      user_email: result.dataValues.user_email,
+      user_category: result.dataValues.user_category,
+    }
+    const data = req.session.userInfo;
       res.send({ result: true, data });
-    }
-    else{
-      res.send({ result: false, message: '비밀번호가 틀렸습니다.' })
-    }
-  } else {
-    res.send({ result: false, message: '존재하는 사용자가 없습니다.' })
-    }
-}
+    } else{
+        res.send({ result: false, message: '비밀번호가 틀렸습니다.' })
+      }
+    } else {
+      res.send({ result: false, message: '존재하는 사용자가 없습니다.' })
+      }
+  }
   
+//회원가입
+exports.postSignup = async (req, res) => {
+  const { user_id, user_pw } = req.body;
+  const id_result = await User.findOne({
+    where: { user_id: user_id }
+  });
+  if (id_result) {
+    res.send({data: false})
+    return ;
+  } else {
+    pw = bcryptPassword(pw);
+    const result = await User.create({
+      id,
+      user_id: req.body.user_id,
+      user_pw: req.body.user_pw,
+      user_name: req.body.user_name,
+      user_addr: req.body.user_addr,
+      user_email: req.body.user_email,
+      user_category: req.body.user_category,
+    });
+  }
+  res.send({result});
+};
+
+// 회원 탈퇴
 exports.patchProfile = async (req, res) => {
   const result = await User.update({
-    name: req.body.name,
-    pw: req.body.pw,
+    user_id: req.body.user_id,
+    user_pw: req.body.user_pw,
+    user_name: req.body.user_name,
+    user_addr: req.body.user_addr,
+    user_email: req.body.user_email,
+    user_category: req.body.user_category,
   }, {
-    where: {userid: req.body.userid}
+    where: {user_id: req.body.user_id}
   });
   res.send({result: true})
 };
   
 exports.deleteUser = async (req, res) => {
-  console.log("req.body:", req.session.userInfo);
   const result = await User.destroy({
-    where: { userid: req.session.userInfo.id },
+    where: { userid: req.session.userInfo.user_id },
   });
   req.session.destroy((err) =>{
     if (err) {
--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const controller = require('../controller/userController');
 
 router.get('/login', controller.getLogin);
-router.post('/login', controller.postLogin);
+router.post('/login', controller.getSignin);
 
 router.get('/signup', controller.getSignup);
 router.post('/signup', controller.postSignup);
 
-router.get('/profile', controller.getProfile);
-router.patch('/profile/edit', controller.patchProfile);
+router.patch('/myPage', controller.patchProfile);
 router.delete('/destroy', controller.deleteUser);
 
 module.exports = router;
