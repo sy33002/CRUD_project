16e4fef3254278136fc70ccb530833de84736e05
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-09-07 14:39:59 +0900

    feat: 프론트 폴더 구조 생성

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 8000;
 
 app.set('view engine', 'ejs');
-app.set('/view', 'views');
-app.set('/static', 'static');
+app.set('views', path.join(__dirname + '/views/pages'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/static', express.static(__dirname + '/public'));
 
 const mainRouter = require('./routes/main');
 app.use('/', mainRouter);
