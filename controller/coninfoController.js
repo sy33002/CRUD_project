b8511fff2c0fdefc647f7484955aa3dc2c7232d9
KimSeEun <cocobell3@naver.com>
2023-09-09 07:34:12 +0900

    feat: routes/conference get요청 작성

--- controller/coninfoController.js
+++ controller/coninfoController.js
@@ -4,3 +4,12 @@ exports.index = (req, res) => {
     // index.ejs 랜더 (data 키로 session 객체의 userInfo 전달)
     res.render('index');
 };
+exports.getConferenceList = (req, res) => {
+    res.render('event/list');
+};
+exports.getConferenceWrite = (req, res) => {
+    res.render('event/write');
+};
+exports.getConferenceDetail = (req, res) => {
+    res.render('event/detail');
+};
