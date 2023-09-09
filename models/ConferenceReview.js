33faaa127543c8b45465daea75d1461473318032
flashrifle <flashrifle@gmail.com>
2023-09-09 17:34:44 +0900

    feat: model utf-8 옵션추가, 리뷰 조회 기능 확인

--- models/ConferenceReview.js
+++ models/ConferenceReview.js
@@ -39,6 +39,8 @@ const ConferenceReview = (Sequelize, sequelize) => {
             tableName: 'con_review', // 실제 db 테이블명
             freezeTableName: true, // 테이블명 고정 (모델 이름 테이블로 바꿀 때 복수형으로 바뀜)
             timestamps: false,
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
         }
     );
     return model;
