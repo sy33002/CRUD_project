4ae11905b4d3c1742dac548a25f47bc22101e9ff
KimSeEun <cocobell3@naver.com>
2023-09-10 00:10:20 +0900

    feat: conference경로수정, client ip모듈 설치, cookie관련 app.js설정, conference 조회수 증가, conference 등록 컨트롤러구현

--- controller/coninfoController.js
+++ controller/coninfoController.js
@@ -1,19 +1,79 @@
 const { Conference, Sequelize } = require('../models'); // ../models/index.js
-
-exports.index = (req, res) => {
-    // index.ejs 랜더 (data 키로 session 객체의 userInfo 전달)
-    res.render('index');
-};
+const cookieParser = require('cookie-parser');
+//ip를 가져오는 함수
+function getUserIP(req) {
+    const addr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    return addr;
+}
 exports.getConferenceList = (req, res) => {
-    res.render('event/list');
+    const conference = Conference.findAll();
+    res.render('event/list', { conference });
 };
 exports.getConferenceWrite = (req, res) => {
     res.render('event/write');
 };
+
 exports.getConferenceDetail = async (req, res) => {
-    const { con_id } = req.params;
-    const result = await Visitor.findOne({
-        where: { con_id },
+    const { conId } = req.params;
+    const result = await Conference.findOne({
+        where: { con_id: conId },
     });
-    res.render('event/detail', { data: result });
+
+    res.render(`event/detail`, { result });
+};
+
+//게시글 등록(DB에 저장까지만~)
+exports.postConference = async (req, res) => {
+    const {
+        conTitle,
+        conStartDate,
+        conEndDate,
+        subStartDate,
+        subEndDate,
+        isOnoff,
+        conLocation,
+        conCategory,
+        conCompany,
+        conIsfree,
+        conPrice,
+        conPeople,
+        conCompanyUrl,
+        conCount,
+    } = req.body;
+    const result = await Conference.create({
+        con_title: conTitle,
+        con_start_date: conStartDate,
+        con_end_date: conEndDate,
+        sub_start_date: subStartDate,
+        sub_end_date: subEndDate,
+        is_onoff: isOnoff,
+        con_location: conLocation,
+        con_category: conCategory,
+        con_company: conCompany,
+        con_isfree: conIsfree,
+        con_price: conPrice,
+        con_people: conPeople,
+        con_company_url: conCompanyUrl,
+        con_count: conCount,
+    });
+    //관리자 페이지 안만들면 /event경로로 갑니다..
+    res.redirect('/');
+    //관리자 페이지가 있을 경우
+    //res.render('관리자페이지',{result})
+};
+exports.updateConferenceCnt = async (req, res) => {
+    if (req.cookies[conId] == undefined) {
+        // key, value, 옵션을 설정해준다.
+        res.cookie(conCount, getUserIP(req), {
+            // 유효시간 : 일주일
+            maxAge: 60 * 60 * 24 * 7,
+        });
+        // 조회수 증가 쿼리
+        await Conference.updateOne(
+            { con_id: conId },
+            { $inc: { con_count: 1 } }
+        );
+    }
+    res.render({ conCount: Conference.con_count });
+    //ejs에서 conCount라는 변수를 써서 조회수를 보이게 하면 될 것 같습니다..
 };
