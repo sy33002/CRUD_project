87bc314efe98d89f464300b2eb2826334575846e
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-09-10 15:43:26 +0900

    feat: 회원가입 페이지 및 로직 수정

--- controller/userController.js
+++ controller/userController.js
@@ -1,102 +1,108 @@
 const { User, Sequelize } = require('../models');
 const { Op } = require('sequelize');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 // login 페이지 렌더
 exports.getLogin = (req, res) => {
-  const data = req.session.userInfo;
-  res.render('login');
+    const data = req.session.userInfo;
+    res.render('login');
 };
 
 // signup 페이지 렌더
 exports.getSignup = (req, res) => {
-  res.render('signup');
+    res.render('signup');
 };
 
 // 로그인
 exports.postLogin = async (req, res) => {
-  const { userId, userPw } = req.body;
-  try{
-    const result = await User.findOne({
-      where: { user_id: userId }
-    });
-    if (result != null) {
-      if (compareFunc(userPw, result.user_pw) === true) {
-        req.session.userInfo = {
-        id: result.dataValues.id,
-        userName: result.dataValues.user_name,
-        userId: result.dataValues.user_id,
-        userPw: result.dataValues.user_pw,
-        userAddr: result.dataValues.user_addr,
-        userEmail: result.dataValues.user_email,
-        userCategory: result.dataValues.user_category,
-      }
-      const data = req.session.userInfo;
-        res.send({ result: true, data });
-      } else{
-          res.send({ result: false, message: '비밀번호가 틀렸습니다.' })
+    const { userId, userPw } = req.body;
+    try {
+        const result = await User.findOne({
+            where: { user_id: userId },
+        });
+        if (result != null) {
+            if (compareFunc(userPw, result.user_pw) === true) {
+                req.session.userInfo = {
+                    id: result.dataValues.id,
+                    userName: result.dataValues.user_name,
+                    userId: result.dataValues.user_id,
+                    userPw: result.dataValues.user_pw,
+                    userAddr: result.dataValues.user_addr,
+                    userEmail: result.dataValues.user_email,
+                    userCategory: result.dataValues.user_category,
+                };
+                const data = req.session.userInfo;
+                res.send({ result: true, data });
+            } else {
+                res.send({ result: false, message: '비밀번호가 틀렸습니다.' });
+            }
+        } else {
+            res.send({ result: false, message: '존재하는 사용자가 없습니다.' });
         }
-      } else {
-        res.send({ result: false, message: '존재하는 사용자가 없습니다.' })
-        }
-  } catch (error) {
-      console.error('로그인 시 DB 조회 오류:', error);
-      res.status(500).send({ result: false, message: '서버 오류가 발생했습니다.' });
-  }
-}
+    } catch (error) {
+        console.error('로그인 시 DB 조회 오류:', error);
+        res.status(500).send({
+            result: false,
+            message: '서버 오류가 발생했습니다.',
+        });
+    }
+};
 
 // 로그 아웃
 exports.postLogout = async (req, res) => {
-  req.session.destroy((err) => {
-    if(err) {
-      return;
-    }
-    res.redirect('/');
-  });
+    req.session.destroy((err) => {
+        if (err) {
+            return;
+        }
+        res.redirect('/');
+    });
 };
 
 //회원가입 id 중복체크
 exports.checkId = async (req, res) => {
-  const { id } = req.params;
+    const { id } = req.params;
     const reuslt = await User.findOne({
-      where: {user_id: id},
-    })
+        where: { user_id: id },
+    });
     if (result != null) {
-      res.send({ result: true, message: '이미 존재하는 아이디 입니다!'});
+        res.send({ result: true, message: '이미 존재하는 아이디 입니다!' });
     } else {
-      res.send({result: false, message: '사용 가능한 아이디입니다!'});
+        res.send({ result: false, message: '사용 가능한 아이디입니다!' });
     }
 };
 
 //회원가입
 exports.postSignup = async (req, res) => {
-  try {
-    const { userId, userPw, userName, userAddr, userEmail, userCategory } = req.body;
-    pw = bcryptPassword(pw);
-    const result = await User.create({
-      id,
-      user_id: userID,
-      user_pw: userPW,
-      user_name: userName,
-      user_addr: userAddr,
-      user_email: userEmail,
-      user_category: userCategory,
-    });
-    res.send({result, message: '회원가입 성공!'});
-  } catch (error) {
-    console.error('회원가입 시 오류:', error);
-    res.status(500).send({ result: false, message: '오류가 발생했습니다.' });
-  }
+    try {
+        const { userId, userPw, userName, userAddr, userEmail, userCategory } =
+            req.body;
+        pw = bcryptPassword(userPw);
+        const result = await User.create({
+            user_id: userId,
+            user_pw: userPw,
+            user_name: userName,
+            user_addr: userAddr,
+            user_email: userEmail,
+            user_category: userCategory,
+        });
+        res.send({ result, message: '회원가입 성공!' });
+    } catch (error) {
+        console.error('회원가입 시 오류:', error);
+        res.status(500).send({
+            result: false,
+            message: '오류가 발생했습니다.',
+        });
+    }
 };
 
 // 비밀번호 암호화 함수
 const saltRounds = 5;
 function bcryptPassword(password) {
-  return bcrypt.hashSync(password, saltRounds);
+    return bcrypt.hashSync(password, saltRounds);
 }
-  
+
 function compareFunc(password, hashedPassword) {
-  return bcrypt.compareSync(password, hashedPassword); 
+    return bcrypt.compareSync(password, hashedPassword);
 }
 
 // 회원 탈퇴
@@ -113,7 +119,7 @@ function compareFunc(password, hashedPassword) {
 //   });
 //   res.send({result: true})
 // };
-  
+
 // exports.deleteUser = async (req, res) => {
 //   const result = await User.destroy({
 //     where: { userid: req.session.userInfo.user_id },
@@ -125,4 +131,3 @@ function compareFunc(password, hashedPassword) {
 //     res.send({result});
 //   })
 // };
-  
