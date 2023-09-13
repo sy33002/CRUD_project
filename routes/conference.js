341535dbc79812c59b7fa44bdc182d27663d6870
flashrifle <flashrifle@gmail.com>
2023-09-13 18:19:47 +0900

    feat: upload 라우터 main 으로 리팩토링

--- routes/conference.js
+++ routes/conference.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const uploadDetail = require('../utils');
 
 const router = express.Router();
 const controller = require('../controller/coninfoController');
@@ -11,14 +10,6 @@ router.get('/event/:id', controller.getConferenceDetail);
 router.post('/event/write', controller.postConference);
 router.put('/event/:id', controller.updateConferenceCnt); //컨퍼런스 조회수 증가
 router.post('/event/:id', controller.postConferenceEdit);
-
-router.post(
-    '/upload/:path',
-    uploadDetail.single('conferenceFile'),
-    (req, res) => {
-        res.send({ result: true, file: req.file });
-    }
-);
 
 module.exports = router;
 // 재민님 일단 제가 DB에 컬럼 만들게요~
