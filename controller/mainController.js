377efb8e6287a48f34f8609d88d6d189d832f743
KimSeEun <cocobell3@naver.com>
2023-09-19 14:39:55 +0900

    feat:main페이지 행사 현재 시간이후에 끝나는 행사만 내보냄

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,8 +1,9 @@
 const { ConferenceReview } = require('../models'); // ../models/index.js
 const { Conference } = require('../models'); // ../models/index.js
-
+const { Sequelize, Op } = require('sequelize');
 exports.index = async (req, res) => {
     const data = req.session.userInfo;
+    const currentDate = new Date();
     const reviews = await ConferenceReview.findAll({
         limit: 10,
         order: [['re_id', 'DESC']],
@@ -10,6 +11,9 @@ exports.index = async (req, res) => {
     const events = await Conference.findAll({
         where: {
             is_agreed: true,
+            con_end_date: {
+                [Op.gte]: currentDate,
+            },
         },
         limit: 10,
         order: [['con_id', 'DESC']],
