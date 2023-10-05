0d6ccd2a46dd49cf5a693c68342df9d141ca4237
KimSeEun <cocobell3@naver.com>
2023-10-05 19:14:44 +0900

    fix: 행사 등록시 행사기간이나 우편번호 유효성검사 걸리면 행사등록 안되는 에러 해결

--- utils/utils.js
+++ utils/utils.js
@@ -1,32 +1,49 @@
-// multer 관련 설정
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-// multer 세부 설정
-const uploadDetail = multer({
-    storage: multer.diskStorage({
-        destination(req, file, done) {
-            const folderPath = `public/images/upload/`;
+const storage = multer.diskStorage({
+    destination(req, file, done) {
+        const folderPath = `public/images/upload/`;
 
-            fs.readdir(folderPath, (err) => {
-                // uploads 폴더 없으면 생성
-                if (err) {
-                    fs.mkdirSync(folderPath, { recursive: true });
-                }
-
-                done(null, `public/images/upload/`);
+        fs.promises
+            .access(folderPath)
+            .then(() => {
+                // uploads 폴더가 이미 존재하는 경우
+                done(null, folderPath);
+            })
+            .catch(() => {
+                // uploads 폴더가 없는 경우, 생성
+                return fs.promises.mkdir(folderPath, { recursive: true });
+            })
+            .then(() => {
+                done(null, folderPath);
+            })
+            .catch((err) => {
+                done(err, null);
             });
-        },
-        filename(req, file, done) {
-            const ext = path.extname(file.originalname);
-            done(
-                null,
-                path.basename(file.originalname, ext) + Date.now() + ext
-            );
-        },
-    }),
+    },
+    filename(req, file, done) {
+        const ext = path.extname(file.originalname);
+        done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+});
+
+const uploadDetail = multer({
+    storage,
     limits: { fileSize: 5 * 1024 * 1024 },
+    fileFilter(req, file, done) {
+        // 파일 유효성 검사 로직을 추가
+        if (
+            file.mimetype === 'image/jpeg' ||
+            file.mimetype === 'image/jpg' ||
+            file.mimetype === 'image/png'
+        ) {
+            done(null, true);
+        } else {
+            done(new Error('File type not supported'), false);
+        }
+    },
 });
 
 module.exports = { uploadDetail };
