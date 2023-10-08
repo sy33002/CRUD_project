7941f8cb3e6dfb7e983a8a1329973dcbe9e423cd
flashrifle <flashrifle@gmail.com>
2023-10-08 13:07:57 +0900

    feat: 이미지 크롤링 개발중

--- controller/crawlingController.js
+++ controller/crawlingController.js
@@ -15,7 +15,20 @@ exports.getCrawling = async (req, res) => {
             const company = $('div[class*="Item_host__"]');
             const date = $('div[class*="Item_date__"]');
             const hashTag = $('div[class*="Item_tags___"]');
+            // const img = $(
+            //     '#__next > div > main > section > div:nth-child(2) > div.list_list__OCUxz > div:nth-child(1) > div > a > div > div.Item_item__content__img__hwo5y > span > img'
+            // ).attr('srcset');
 
+            const img = $('div.Item_item__content__img__hwo5y > span > img');
+
+            const imgList = [];
+            const imgSrc = img.each(function (idx, element) {
+                const src = $(element).attr('currentSrc');
+                // const absoluteSrc = new URL(src, url).toString(); // 상대 경로를 절대 경로로 변환
+                // console.log('이미지: ', imgSrc);
+                imgList.push(src);
+            });
+            console.log('imgList:', img);
             // Title과 Company 정보를 담을 배열을 선언합니다.
             const eventList = [];
 
@@ -25,13 +38,15 @@ exports.getCrawling = async (req, res) => {
                 const companyText = $(company[i]).text();
                 const dateText = $(date[i]).text();
                 const hashTagText = $(hashTag[i]).text();
+                const imgText = imgList[i];
+                // console.log('IMGText', imgText);
 
                 // 날짜 문자열에서 시작일과 종료일을 추출합니다.
                 const [startDateStr, endDateStr] = dateText.split(' ~ ');
 
                 // ISO 형식의 날짜로 변환
-                const startDate = moment(startDateStr);
-                const endDate = moment(endDateStr);
+                const startDate = moment(startDateStr, 'YYYYMMDD');
+                const endDate = moment(endDateStr, 'YYYYMMDD');
 
                 const hashTagSplit = hashTagText.split(/[\s#]+/);
                 if (hashTagSplit[0] === '') {
@@ -43,13 +58,14 @@ exports.getCrawling = async (req, res) => {
                     con_start_date: startDate.format('YYYY-MM-DD'), // YYYY-MM-DD 형식
                     con_end_date: endDate.format('YYYY-MM-DD'), // YYYY-MM-DD 형식
                     hashTag: hashTagSplit,
+                    imgSrc: imgText,
                 };
 
                 eventList.push(event);
             }
 
             // 배열에 저장된 객체를 출력합니다.
-            console.log(eventList);
+            // console.log(eventList);
             // res.send(eventList);
             res.render('crawling/crawling', { data: eventList });
         } catch (error) {
